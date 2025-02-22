366412c93afd7ed8d67a23320756ced64277cd72
okhusinov <okhusinov@chicagobooth.edu>
2025-02-22 23:01:03 +0000

    added 2d10

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -2,9 +2,13 @@ class DiceController < ApplicationController
 
   def two_six
 
-
-
     render ({ :template => "dice_templates/2d6" })
+  
+  end
+
+  def two_ten
+
+    render ({ :template => "dice_templates/2d10" })
   
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
   get("/dice/2/6", { :controller => "dice", :action => "two_six" })
+  get("/dice/2/10", { :controller => "dice", :action => "two_ten" })
  
 end
