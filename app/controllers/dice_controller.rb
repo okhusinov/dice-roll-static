91510ccb62569fb2330450b4b44a8c9b998d9b85
okhusinov <okhusinov@chicagobooth.edu>
2025-02-22 23:44:39 +0000

    finalss

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -12,6 +12,24 @@ class DiceController < ApplicationController
   
   end
 
+  def one_twenty
+
+    render ({ :template => "dice_templates/1d20" })
+  
+  end
+
+  def five_four
+
+    @dice_rolls = Array.new
+    5.times do
+      @new_roll = rand(1..4)
+      @dice_rolls.push(@new_roll)  
+    end
+    
+    render ({ :template => "dice_templates/5d4" })
+  
+  end
+
   def homepage
 
     render ({ :template => "dice_templates/home" })
